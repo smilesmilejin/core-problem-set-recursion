bb594d2fcaf352514adbc3046977e43a09313e5c
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-02 01:43:53 -0400

    Completed Part 1 and 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,58 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError('The input n must be greater than or equal to 0')
+    
+    if n == 0:
+        return 1
+    
+    return n * factorial(n-1)
 
 # reverse
+count = 0
+def reverse(text):
+    if len(text) == 1:
+        return text
+    if not text:
+        return ""
+    
+    global count
+    
+    text_list = list(text)
 
+    if count == len(text) // 2:
+        return ''.join(text_list)
+    
+    temp = text_list[count]
+    text_list[count]= text_list[- count - 1]
+    text_list[- count - 1] = temp
 
+    count += 1
+
+    return reverse(text_list)
 
 # bunny
+def bunny(count):
+    if count == 0:
+        return 0 
 
-
+    return bunny(count-1) + 2
 
 # is_nested_parens
+def is_nested_parens(parens):
+    return calculate_count(parens)
 
+def calculate_count(parens, count=0, index=0):
+    if index == len(parens):
+        return count == 0
+    if count < 0:
+        return False
+    
+    if parens[index] == '(':
+        count += 1
+    elif parens[index]== ')':
+        count -= 1
 
+    return calculate_count(parens, count, index+1)
--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    
+    if array[0] == query:
+        return True
+    
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 0 or len(text) == 1:
+        return True
+    
+    if text[0] != text[-1]:
+        return False
+    
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(num1, num2):
+    return calculate_match(num1, num2)
+
+def calculate_match(num1, num2, match=0):
+    if (num1 % 10 == num1) or (num2 % 10 == num2):
+        if num1 % 10 == num2 % 10:
+            match += 1
+        return match      
+    
+    if num1 % 10 == num2 % 10:
+        match += 1
+    
+    return calculate_match(num1 // 10, num2 // 10, match)
 
 
